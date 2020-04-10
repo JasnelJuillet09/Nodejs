f0e7ab5b0b370be699972a77a46622be111cd219
Jasnel Juillet <62491684+JasnelJuillet09@users.noreply.github.com>
2020-04-09 20:24:03 -0500

    Add files via upload

---
+++ time-server.js
@@ -0,0 +1,21 @@
+var net = require('net');
+ 
+function zeroFill(i) {
+   if(i < 10) return '0' + i;
+   else return '' + i;
+}
+ 
+function completDate() {
+    var d = new Date();
+    return d.getFullYear() + '-'
+        + zeroFill(d.getMonth() + 1) + '-'
+        + zeroFill(d.getDate()) + ' '
+        + zeroFill(d.getHours()) + ':'
+        + zeroFill(d.getMinutes());
+}
+ 
+var server = net.createServer(function (socket) {
+    socket.end(completDate() + '\n');
+});
+ 
+server.listen(Number(process.argv[2]));
